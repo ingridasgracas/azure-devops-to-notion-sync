81bf0e710d5e9526c122f69b68caa2033621397b
Ingrid <67933021+ingridasgracas@users.noreply.github.com>
2025-05-26 18:52:55 -0300

    Update test_azure_tokes.py

--- test_azure_tokes.py
+++ test_azure_tokes.py
@@ -1,8 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-url = url = "https://dev.azure.com/tjerj/_apis/projects?api-version=6.0"
-token = "363p82D7ClfhKBAKeoSsipwPTLN40j0smFuMg5ZomVcoOW55XrAuJQQJ99BEACAAAAAsfoTXAAASAZDO3uGg"
+url = url = ""
+token = ""
 
 
 response = requests.get(url, auth=HTTPBasicAuth('', token))
